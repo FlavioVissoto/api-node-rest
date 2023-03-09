ccd4d9f679e18002c38e60e6e7eb675ce5a9d965
FlavioVissoto <vissoto_flavio@hotmail.com>
2023-03-09 17:17:33 -0300

    Feat: DigitalOcean configs

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,5 +2,3 @@ import supertest from 'supertest';
 import { server } from '../src/server/server';
 
 export const testServer = supertest(server);
-
-testServer.get('/pokemon/getall').send({ f: 'f' });
