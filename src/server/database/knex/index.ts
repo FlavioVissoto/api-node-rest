ead321f32b352ae234f40873970e53847ce20936
FlavioVissoto <vissoto_flavio@hotmail.com>
2023-03-23 18:10:33 -0300

    Feat: Config ambiente produção e banco de dados.

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,6 +1,15 @@
+import 'dotenv/config';
+
 import { development, production, test } from './environment';
 
 import { knex } from 'knex';
+import pg from 'pg';
+
+if (process.env.NODE_ENV === 'production') {
+  pg.types.setTypeParser(pg.types.TypeId.INT2, 'text', parseInt);
+  pg.types.setTypeParser(pg.types.TypeId.INT4, 'text', parseInt);
+  pg.types.setTypeParser(pg.types.TypeId.INT8, 'text', parseInt);
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {
