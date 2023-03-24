51bac5bc2586e6ba552a35a4f830927f88127f59
FlavioVissoto <vissoto_flavio@hotmail.com>
2023-03-24 10:37:57 -0300

    Feat: CORS

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import './shared/services/yup.translations';
 
+import cors from 'cors';
 import express from 'express';
 import { router } from './routes';
 import { morganMiddleware } from './shared/middleware';
 
 const server = express();
+
+server.use(cors({ origin: '*' }));
 
 server.use(morganMiddleware);
 
